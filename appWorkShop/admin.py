21481a530e765d32fcc6159fd234467a4700e694
Felipe Herrera <felipe.ah14@gmail.com>
2019-06-26 18:48:07 -0400

    primer commit

---
+++ appWorkShop/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register( Colegio )
+admin.site.register( Alumno )
+admin.site.register( Profesor )
+admin.site.register( Taller )
---
+++ appWorkShop/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+class Colegio(models.Model):
+    nombreColegio = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.nombreColegio)
+
+class Alumno(models.Model):
+    rut = models.CharField(max_length=10, primary_key=True)
+    nombreCompleto = models.CharField(max_length=100)
+    colegio = models.ForeignKey('Colegio', on_delete=models.CASCADE)
+    edad = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.rut)
+
+class Profesor(models.Model):
+    rutProfe = models.CharField(max_length=10, primary_key=True)
+    nombreProfe = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return str(str.rutProfe)
+
+class Taller(models.Model):
+    idTaller = models.AutoField(primary_key=True)
+    nombreTaller = models.CharField(max_length=40)
+    alumno = models.ManyToManyField( Alumno )
+    profesor = models.ManyToManyField( Profesor )
+    colegio = models.ManyToManyField( Colegio )
+
+    def __str__(self):
+        return str(str.nombreTaller)
