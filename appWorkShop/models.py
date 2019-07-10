ea788c9686bb81558bc390ce9dd42fa9053a084c
Felipe Herrera <felipe.ah14@gmail.com>
2019-07-10 00:42:14 -0400

    cambio subiado para ver pagina

--- appWorkShop/models.py
+++ appWorkShop/models.py
@@ -1,7 +1,13 @@
 from django.db import models
-import datetime
 
-# Create your models here.
+DIAS_SEMANA = ( 
+    ('Lunes', 'Lunes'),
+    ('Martes', 'Martes'),
+    ('Miercoles', 'Miercoles'),
+    ('Jueves', 'Jueves'),
+    ('Viernes', 'Viernes'),
+
+)
 
 class Colegio(models.Model):
     nombreColegio = models.CharField(max_length=100)
@@ -20,7 +26,8 @@ class Alumno(models.Model):
 
 class Profesor(models.Model):
     rutProfe = models.CharField(max_length=10, primary_key=True)
-    nombreProfe = models.CharField(max_length=100)
+    nombreProfe = models.CharField(max_length=100, null=True, blank=True)
+    apellidoProfe = models.CharField(max_length=100, null=True, blank=True)
     
     def __str__(self):
         return str(self.rutProfe)
@@ -28,10 +35,20 @@ class Profesor(models.Model):
 class Taller(models.Model):
     idTaller = models.AutoField(primary_key=True)
     nombreTaller = models.CharField(max_length=40)
-    alumno = models.ManyToManyField( Alumno )
-    profesor = models.ManyToManyField( Profesor )
-    colegio = models.ManyToManyField( Colegio )
-    fecha = models.DateTimeField(blank=False, null=True)
+    alumno = models.ManyToManyField( Alumno , null=True, blank=True)
+    profesor = models.ManyToManyField( Profesor, null=True, blank=True)
+    colegio = models.ManyToManyField( Colegio , null=True, blank=True )
+    fecha = models.CharField(max_length=12, choices=DIAS_SEMANA, default="Lunes")
+    hora_comienzo = models.TimeField (null=True, blank=True)
+    duracion_total= models.CharField( max_length=40, null=True, blank=True)
 
     def __str__(self):
         return str(self.nombreTaller)
+
+class TallerAlumno(models.Model):
+    id_tomado = models.AutoField(primary_key=True)
+    tallerTomado = models.ForeignKey( Taller , null=True, blank=True,on_delete=models.CASCADE)
+    alumnoTomado = models.ForeignKey( Alumno , null=True, blank=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.tallerTomado)
--- appWorkShop/views.py
+++ appWorkShop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from .models import Profesor, Alumno, Taller
 from django.contrib.auth.models import User
@@ -10,11 +10,14 @@ def login(request):
     return render(request, 'iworkshop/login.html', {})
 
 def home(request):
-    return render(request, 'iworkshop/home.html', {})
+    q = request.GET.get('q','')
+    talleres = Taller.objects.order_by('hora_comienzo').filter(alumno__rut__contains=q)
+    return render(request, 'iworkshop/home.html', {'talleres': talleres})
 
 def buscarAlum(request):
     talleres = Taller.objects.all()
     return render(request, 'iworkshop/buscarAlum.html', {'talleres': talleres})
+
 
 def results(request):
     q = request.GET.get('q','')
